fc47284f86463b25ebca658395a6df938efd9aad
louisng114 <92226924+louisng114@users.noreply.github.com>
2024-05-09 20:19:41 -0700

    Fix error handling

--- app.js
+++ app.js
@@ -92,6 +92,18 @@ app.get("/all", (req, res) => {
     return res.json(result);
 });
 
+app.use(function (req, res, next) {
+    const notFoundError = new ExpressError("Not Found", 404);
+    return next(notFoundError)
+});
+
+app.use(function(err, req, res, next) {
+    let status = err.status || 500;
+    let message = err.message;
+    return res.status(status).json({
+        error: {message, status}
+});
+
 
 app.listen(3000, function(){
     console.log('App on port 3000');
